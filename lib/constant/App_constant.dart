fcc0c2a265a526982e956ddbd8d27a23cc52128b
Abdullah Akram <108747089+aharoo786@users.noreply.github.com>
2025-11-26 14:55:16 +0500

    app submitted
    app submitted

--- lib/constant/App_constant.dart
+++ lib/constant/App_constant.dart
@@ -1,11 +1,11 @@
 class AppConstants {
   // App Share Links
   static const String playStoreUrl = 'https://play.google.com/store/apps/details?id=com.yourapp.package';
-  static const String appStoreUrl = 'https://apps.apple.com/app/idYOUR_APP_ID';
+  static const String appStoreUrl = 'https://apps.apple.com/us/app/speech-therapy-world/id6754527860';
   
   // Feedback/Rating Links (Direct to Review Page)
   static const String playStoreFeedbackUrl = 'https://play.google.com/store/apps/details?id=com.yourapp.package';
-  static const String appStoreFeedbackUrl = 'https://apps.apple.com/app/idYOUR_APP_ID?action=write-review';
+  static const String appStoreFeedbackUrl = 'https://apps.apple.com/us/app/speech-therapy-world/id6754527860?action=write-review';
   
   // Share Message (For WhatsApp, SMS, etc.)
   static const String shareMessage = '''Check out this amazing app! Download it now:
